801c29624535b6e36412c5b28479d7def0ddf89a
rain46688 <rain46688@gmail.com>
2022-08-16 17:24:20 +0900

    3, 리액트에서 레이아웃 만들 때 쓰는 JSX 문법

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 이건 JSX라는 언어임 
+      className이라고 적어야됨!
+      */}
+      <div className="black-nav">
+        {/*  스타일 코드 짤때 이런식으로 key value로 넣어야됨, - 이어지는건 카멜 케이스로 변환해야됨 */}
+        <h4 style={{color : 'red', fontSize : '16px'}}>블로그임</h4>
+      </div>
+      {/* 중괄호를 써서 이렇게 변수를 집어넣을수 있음, 데이터 바인딩이라는것 */}
+      <h4>{ post }</h4>
     </div>
   );
 }
