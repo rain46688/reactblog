d5d5d0dbf809d9a8079e00fd4b084ac371ee4c9a
rain46688 <rain46688@gmail.com>
2022-08-16 17:39:17 +0900

    4, state 사용법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let blog = 'ReactBlog';
+  // state를 사용하는건 변동시 자동으로 html에 반영되게 만들고
+  // 싶으면 state를 사용한다.
+  let [글제목, b] = useState(['남자 코트 추천','여자 코드 추천','남자 가디건 추천']);
+  // 앞에는 state 저장한 변수가 담겨지고, 뒤에 변수에는 함수가 담겨짐
+ // let [a,b] = [1,2] destructuring 문법이있다고함 a에1이 들어가고 b에 2가 들어감
+ // useState하면 저런 형식으로 데이터가 남게되고 해당 방식으로 담겨짐
+ 
 
   return (
     <div className="App">
@@ -12,10 +20,24 @@ function App() {
       */}
       <div className="black-nav">
         {/*  스타일 코드 짤때 이런식으로 key value로 넣어야됨, - 이어지는건 카멜 케이스로 변환해야됨 */}
-        <h4 style={{color : 'red', fontSize : '16px'}}>블로그임</h4>
+        <h4 style={{color : 'red', fontSize : '16px'}}>{ blog }</h4>
       </div>
-      {/* 중괄호를 써서 이렇게 변수를 집어넣을수 있음, 데이터 바인딩이라는것 */}
-      <h4>{ post }</h4>
+     
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+
     </div>
   );
 }
